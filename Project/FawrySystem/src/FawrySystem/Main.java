7a97e64b33c58acf96d5b85b3191534be06e85ff
AlaaAymanHashem <117596345+AlaaAymanHashem@users.noreply.github.com>
2022-12-04 14:32:05 +0200

    refund

--- Project/FawrySystem/src/FawrySystem/Main.java
+++ Project/FawrySystem/src/FawrySystem/Main.java
@@ -12,19 +12,21 @@ public class Main {
 		ServiceFactory s = new Services();
 		InternetPayment i = new We();
 		double cost = s.payForInternet(i);
-		 String name = in.nextLine();
-		 double money = in.nextDouble();
-		 PaymentService obj = new PaymentService();
-		 Payment obj2 = new CreditCard(name, money);
-		 obj.setStrategy(obj2);
-		 obj.ProcessOrder(obj2.pay(cost));
-		 obj2.account(50.0);
-		 ServiceFactory ss = new Services();
-		 MobileRecharge m = new Orange();
-		 cost = ss.payForMobile(m);
+		String name = in.nextLine();
+		double money = in.nextDouble();
+		PaymentService obj = new PaymentService();
+		Payment obj2 = new CreditCard(name, money);
+		obj.setStrategy(obj2);
+		obj.ProcessOrder(obj2.pay(cost));
+		obj2.account(50.0);
+		ServiceFactory ss = new Services();
+		MobileRecharge m = new Orange();
+		cost = ss.payForMobile(m);
 		Refund r = new Refund();
 		r.RefundRequest();
-		//r.getMoney();
+		r.getMoney();
+		Wallet w = new Wallet(10);
+		System.out.println(w.availMoney);
 	}
 
 	
--- Project/FawrySystem/src/FawrySystem/Refund.java
+++ Project/FawrySystem/src/FawrySystem/Refund.java
@@ -5,13 +5,14 @@ import java.util.*;
 public class Refund {
     Scanner input = new Scanner(System.in);
 
-    Payment p = new Wallet();
+    Wallet p = new Wallet();
     ServiceFactory s;
     boolean status = true;
 
     public boolean RefundRequest() {
         System.out.println("Enter ID of the service: ");
         String ID = input.next();
+
         // send refund request to admin
         System.out.println("your refund request is sent to the admin.");
         if (status == true) {
@@ -24,11 +25,12 @@ public class Refund {
     }
 
     public double getMoney() {
-        if (status == true) {
-            p.availMoney += s.servz.get(id);
+        if (status == true) 
+        {
+            p.availMoney += s.servs.get(s.servID);
         }
 
-        return 0;
+        return p.availMoney;
     }
 
 }
