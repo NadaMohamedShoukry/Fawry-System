08322c10c60d21a87df455c40458024fe4ad994f
Judy1692001 <74277086+Judy1692001@users.noreply.github.com>
2022-12-04 23:12:47 +0200

    Update

--- Project/FawrySystem/src/FawrySystem/CreditCard.java
+++ Project/FawrySystem/src/FawrySystem/CreditCard.java
@@ -1,6 +1,9 @@
 package FawrySystem;
 
+import java.util.Scanner;
+
 public class CreditCard implements Payment{
+	Scanner input = new Scanner(System.in);
 	String cardnumber;
 	int Month;
 	int Year;
@@ -13,11 +16,13 @@ public class CreditCard implements Payment{
 		this.availMoney=availMoney;
 		this.amount=amount;
 	}
+	@Override
      public double pay(double amount) {
     	 System.out.println(" Your Account Now " +  (availMoney-amount));
     	 return availMoney;
      }
      
+	@Override
      public boolean validatePay() {
     	 if(availMoney > amount) {
     		 System.out.println("Money in Your Account : "+ (availMoney));
@@ -30,8 +35,28 @@ public class CreditCard implements Payment{
     		
      }
      
-//     public void account(double amount) {
-//    	 System.out.println(" Your Account Now " +  (availMoney-amount));
-//    	 
-//     }
+	@Override
+     public void account(double amount) {
+    	 System.out.println(" Your Account Now " +  (availMoney-amount));
+    	 
+     }
+	
+	@Override
+	public double collectPayDet()
+	{
+		System.out.println("Enter Card No.:");
+		input = new Scanner(System.in);
+		cardnumber=input.next();
+		System.out.println("Enter Month:");
+		input = new Scanner(System.in);
+		Month=input.nextInt();
+		System.out.println("Enter Year:");
+		input = new Scanner(System.in);
+		Year=input.nextInt();
+		System.out.println("Enter Price:");
+		input = new Scanner(System.in);
+		amount=input.nextDouble();
+		
+		return availMoney - amount;
+	}
 }
