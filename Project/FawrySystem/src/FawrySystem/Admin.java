a1c309c77b36f677c51c715305894bb371874001
AlaaAymanHashem <117596345+AlaaAymanHashem@users.noreply.github.com>
2022-12-05 13:27:06 +0200

    .

--- Project/FawrySystem/src/FawrySystem/Admin.java
+++ Project/FawrySystem/src/FawrySystem/Admin.java
@@ -5,10 +5,7 @@ import java.util.*;
 public class Admin {
     Scanner input = new Scanner(System.in);
     Services serv = new Services();
-    boolean status = input.nextBoolean();
-
     
     ArrayList<Refund> refundRequests = new ArrayList<>();
-
 
 }
