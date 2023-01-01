942b3d12285a38ac3e5486adbec2e09e75301b56
Judy1692001 <74277086+Judy1692001@users.noreply.github.com>
2023-01-01 19:00:11 +0200

    move UserDB to User

--- fawry.phase2/fawry.phase2/src/main/java/com/project/software2/fawry/phase2/user/UserDB.java
+++ fawry.phase2/fawry.phase2/src/main/java/com/project/software2/fawry/phase2/user/UserDB.java
@@ -1,7 +1,5 @@
-package com.project.software2.fawry.phase2.DB;
+package com.project.software2.fawry.phase2.user;
 import java.util.ArrayList;
-
-import com.project.software2.fawry.phase2.user.*;
 
 public class UserDB {
 static public ArrayList<User> database = new ArrayList<>();
